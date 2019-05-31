082759bfeda890f2b08051fc3b1f772da1307271
doulos76 <inojyes@gmail.com>
2019-05-31 15:42:10 +0900

    add project files

---
+++ AppleMusicVideoSearchApp/AppleMusicVideoSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusicVideoSearchApp
+//
+//  Created by dave76 on 31/05/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
